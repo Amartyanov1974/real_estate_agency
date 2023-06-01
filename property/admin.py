63cef084caafd3f57ecdcccb38df259e0aa5bfa8
Amartyanov1974 <randomquake@yandex.ru>
2023-06-01 16:55:19 +0400

    Добавил list_display list_editable search_fields readolny_fields

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Flat
+from property.models import Flat
 
+class FlatAdmin(admin.ModelAdmin):
+    list_display = ('town','address', 'price', 'owner', 'new_building', 'construction_year')
+    list_editable =['new_building',]
+    search_fields = ('town','address','owner',)
+    readonly_fields = ["created_at"]
 
-admin.site.register(Flat)
+admin.site.register(Flat, FlatAdmin)
