e07d395b3c92e21ff986e9c3eaeb75c204cb840b
Amartyanov1974 <randomquake@yandex.ru>
2023-06-02 10:59:53 +0400

    Добавил библиотеку и поле для хранения номера телефона

--- property/models.py
+++ property/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
+        blank=True, null=True)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField(
         'Когда создано объявление',
